d54eb6f664edc88891fa309505cafaa777c063a1
Hendrik Kunert <hendrik.kunert@gmail.com>
2022-10-30 21:48:56 +0100

    Bump up env_logger version

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ use koppeln::{
 
 #[tokio::main]
 async fn main() {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let settings = Settings::load().expect("Could not load settings.");
     debug!("Settings:\n{:?}", settings);
